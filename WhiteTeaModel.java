eb0c2750f758cee04994afca80175f7c08300746
凌森鹏 <2428517923@qq.com>
2020-06-02 15:27:37 +0800

    Create WhiteTeaModel.java

---
+++ WhiteTeaModel.java
@@ -0,0 +1,32 @@
+public class WhiteTeaModel extends TeaModel {
+
+    @Override
+    public void baking() {
+        System.out.println("白茶：采摘");
+    }
+
+    @Override
+    public void deterioration() {
+        System.out.println("白茶：萎调");
+    }
+
+    @Override
+    public void rolling() {
+        System.out.println("白茶：揉捻");
+    }
+
+    @Override
+    public void dry() {
+        System.out.println("白茶：烘干");
+    }
+
+    @Override
+    public void fermentation() {
+        System.out.println("白茶：发酵");
+    }
+
+    @Override
+    public void save() {
+        System.out.println("白茶：保存");
+    }
+}
