35b57c471f1cce0308ea913bc562e8e1e82e0fd4
凌森鹏 <2428517923@qq.com>
2020-06-02 15:23:42 +0800

    Create AppConfigs.java

---
+++ AppConfigs.java
@@ -0,0 +1,27 @@
+public class AppConfigs   {
+
+    /**
+     * 采摘
+     */
+    public static final String BAKING = "baking";
+    /**
+     * 萎调
+     */
+    public static final String DETERIORATION = "deterioration";
+    /**
+     * 揉捻
+     */
+    public static final String ROLLING = "rolling";
+    /**
+     * 烘干
+     */
+    public static final String DRY = "dry";
+    /**
+     * 发酵
+     */
+    public static final String FERMENTATION = "fermentation";
+    /**
+     * 保存
+     */
+    public static final String SAVE = "save";
+}
